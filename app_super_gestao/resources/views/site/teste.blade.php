216811ef06cc1a0d81619ece0d76cab877d00e38
alexzarp <alexszds@gmail.com>
2021-05-29 16:16:03 -0300

    Update

---
+++ app_super_gestao/resources/views/site/teste.blade.php
@@ -0,0 +1,2 @@
+Teste p1 = {{$p1}} <br>
+Teste p2 = {{$p2}}
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -34,19 +34,17 @@ Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
 Route::prefix('/app')->group (function () {
     Route::get('/clientes', function () { return "Clientes"; })->name('app.clientes');
-    Route::get('/fornecedores', function () { return "Fornecedores"; })->name('app.fornecedores');;
-    Route::get('/produtos', function () { return "Produtos"; })->name('app.produtos');;
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function () { return "Produtos"; })->name('app.produtos');
 });
 
 Route::get('/login', function () { return "Login"; });
 
-Route::get('/rota1', function () {
-    echo 'rota 1';
-})->name('site.rota1');
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
-Route::get('/rota2', function () {
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
+// Route::get('/rota2', function () {
+//     return redirect()->route('site.rota1');
+// })->name('site.rota2');
 
 // Route::redirect('/rota2','/rota1');
 
