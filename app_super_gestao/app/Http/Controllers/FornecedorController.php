c582fad5e2391a013840c66bee61a2f70a4f9d6a
alexzarp <alexszds@gmail.com>
2021-05-30 16:34:57 -0300

    a

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -8,7 +8,8 @@ class FornecedorController extends Controller
 {
     public function index () {
         $fornecedores = [
-            0 => ['nome' => 'Fornecedor 1', 'status' => 'N']
+            0 => ['nome' => 'Fornecedor 1', 'status' => 'N'],
+            1 => ['nome' => 'Fornecedor 2', 'status' => 'S', 'CNPJ' => 611565651]
         ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -24,8 +24,57 @@
 @endif
 <br>
 @isset($fornecedores)
+{{-- invesão de resultado true - false --}}
     @unless ($fornecedores[0]['status'] == 'S')
         Fornecedor inativo 
     @endunless
 @endisset   
 <br>
+{{-- @empty 
+    ''
+    0
+    0.0
+    '0'
+    null
+    false
+    array[]
+    $var
+    --}}
+@isset($fornecedores)
+    CNPJ: {{$fornecedores[1]['CNPJ'] ?? "Dado vazio"}} <br>
+    cnpj: {{$fornecedores[1]['cnpj'] ?? "Dado vazio"}}
+@endisset
+
+{{-- @switch($type)
+    @case(1)
+        
+        @break
+    @case(2)
+        
+        @break
+    @default
+        
+@endswitch --}}
+
+{{-- @for ($i = 0; $i < $count; $i++)
+    
+@endfor --}}
+
+{{-- @foreach ($collection as $item)
+    
+@endforeach --}}
+
+{{-- @forelse ($collection as $item)
+    Iteração atual: {{ $loop->iteration }}
+    @if($loop->first)
+        true
+    @endif
+
+    @if($loop->last)
+        true
+    @endif
+@empty
+    
+@endforelse --}}
+
+{{-- @--> equivalência a \ --}}
