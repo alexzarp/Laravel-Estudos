25b6b39af0ec3a4641f607ded4e81e22d186f707
alexzarp <alexszds@gmail.com>
2021-05-30 15:58:34 -0300

    Updates

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -4,4 +4,28 @@
 
 @php
     // Teste de comentário nativo
+    // if () 
+    // elseif ( ) {
+    //     # code...
+    // }
+    // else {
+    //     # code...
+    // }
 @endphp
+
+{{-- @dd($fornecedores) --}}
+
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Exitem alguns fornecedores cadastrados</h3>
+@elseif (count($fornecedores) > 10)
+    <h3>Exitem vários fornecedores</h3>
+@else 
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
+<br>
+@isset($fornecedores)
+    @unless ($fornecedores[0]['status'] == 'S')
+        Fornecedor inativo 
+    @endunless
+@endisset   
+<br>
